cdfe14ea7a329e7c33b7ca917eb1fb3325eaf06b
gglnyrdm <yardimoglugulin@gmail.com>
2024-04-25 17:54:03 +0300

    add welcome directive

--- src/app/routers/create-model-page/create-model-page.component.ts
+++ src/app/routers/create-model-page/create-model-page.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CreateModelFormComponent } from '../../features/models/components/create-model-form/create-model-form.component';
+import { WelcomeDirective } from '../../core/directives/welcome.directive';
 
 @Component({
   selector: 'app-create-model-page',
   standalone: true,
   imports: [
     CommonModule,
-    CreateModelFormComponent
+    CreateModelFormComponent,
+    WelcomeDirective
   ],
   templateUrl: './create-model-page.component.html',
   styleUrl: './create-model-page.component.scss',
